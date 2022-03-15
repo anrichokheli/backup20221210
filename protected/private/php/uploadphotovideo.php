922c75a0d5a9adf1911f86e320a86c13e568d3f7
ქვეითი SOS! / Pedestrian SOS! <pedestrian.sos@gmail.com>
2022-03-16 00:40:55 +0400

    Add files via upload

--- protected/private/php/uploadphotovideo.php
+++ protected/private/php/uploadphotovideo.php
@@ -98,41 +98,41 @@
                 if(($uploadedFilesQuantity - $fileIndex) != 1){
                     return;
                 }
-                if(isset($_POST["ps"]))    {
-                    exit(str_replace("</h1>", "</h1><div style=\"border:2px solid #00ff00;\">upload completed<br><a target=\"_blank\" href=\"?" . $GLOBALS["filesQuantity"] . "\">view upload</a></div>", file_get_contents("ps/index.html")));
-                }
                 $key = getKey(1000);
                 file_put_contents(keysPath . $GLOBALS["filesQuantity"], password_hash($key, PASSWORD_DEFAULT));
                 //header("Location: view.php?n=" . $filesQuantity);
-                if(isset($_POST["submitform"]) || isset($_POST["submit"]))    {
-                    if($GLOBALS["lang"] != defaultLang)    {
-                        $langget = "&lang=" . $GLOBALS["lang"];
+                if(isset($_POST["submitform"]) || isset($_POST["submit"]) || isset($_POST["ps"]))    {
+                    if(isset($_POST["ps"]))    {
+                        echo(str_replace("</h1>", "</h1><div style=\"border:2px solid #00ff00;\">upload completed<br><a target=\"_blank\" href=\"?" . $GLOBALS["filesQuantity"] . "\">view upload</a></div>", file_get_contents("ps/index.html")));
+                        echo '<div id="afterupload"></div>';
                     }else{
-                        $langget = "";
+                        if($GLOBALS["lang"] != defaultLang)    {
+                            $langget = "&lang=" . $GLOBALS["lang"];
+                        }else{
+                            $langget = "";
+                        }
+                        $descriptionHTML = file_get_contents(htmlPath . "uploaddescription.html");
+                        $voiceHTML = file_get_contents(htmlPath . "uploadvoice.html");
+                        if(isset($_POST["submit"])){
+                            $noscript = "noscript";
+                            $descriptionHTML = str_replace("<form", "<form action=\"?noscript" . $langget . "\"", $descriptionHTML);
+                            $voiceHTML = str_replace("<form", "<form action=\"?noscript" . $langget . "\"", $voiceHTML);
+                        }else{
+                            $noscript = "";
+                        }
+                        $html = "<div class=\"boxs\" id=\"afterupload\">";
+                        $html .= "<div class=\"texts\">#: " . $GLOBALS["filesQuantity"] . "</div><a href=\"?" . $GLOBALS["filesQuantity"] . $langget . "\" target=\"_blank\" class=\"buttons afteruploadbuttons viewuploadsbuttons\"><img width=\"32\" height=\"32\" src=\"images/viewicon.svg\">&nbsp;<span><string>viewupload</string></span></a><br><br>";
+                        $html .= str_replace("value_n", $GLOBALS["filesQuantity"], str_replace("value_key", $key, $descriptionHTML));
+                        $html .= "<br><br>";
+                        $html .= str_replace("value_n", $GLOBALS["filesQuantity"], str_replace("value_key", $key, $voiceHTML));
+                        $html .= "</div>";
+                        $html = str_replace("<!--AFTER_UPLOAD-->", $html, str_replace("<!--UPLOAD_RESPONSE-->", "<div class=\"texts\" style=\"border:1px solid #00ff00;padding:1px;\"><string>uploadcompleted</string></div><br>", file_get_contents(htmlPath . "index" . $noscript . ".html")));
+                        $html = str_replace("<htmllang>lang</htmllang>", $GLOBALS["lang"], $html);
+                        $html = setLanguage($html);
+                        $html = str_replace("<php>LANG</php>", $langget, $html);
+                        echo $html;
                     }
-                    $descriptionHTML = file_get_contents(htmlPath . "uploaddescription.html");
-                    $voiceHTML = file_get_contents(htmlPath . "uploadvoice.html");
-                    if(isset($_POST["submit"])){
-                        $noscript = "noscript";
-                        $descriptionHTML = str_replace("<form", "<form action=\"?noscript" . $langget . "\"", $descriptionHTML);
-                        $voiceHTML = str_replace("<form", "<form action=\"?noscript" . $langget . "\"", $voiceHTML);
-                    }else{
-                        $noscript = "";
-                    }
-                    $html = "<div class=\"boxs\" id=\"afterupload\">";
-                    $html .= "<div class=\"texts\">#: " . $GLOBALS["filesQuantity"] . "</div><a href=\"?" . $GLOBALS["filesQuantity"] . $langget . "\" target=\"_blank\" class=\"buttons afteruploadbuttons viewuploadsbuttons\"><img width=\"32\" height=\"32\" src=\"images/viewicon.svg\">&nbsp;<span><string>viewupload</string></span></a><br><br>";
-                    $html .= str_replace("value_n", $GLOBALS["filesQuantity"], str_replace("value_key", $key, $descriptionHTML));
-                    $html .= "<br><br>";
-                    $html .= str_replace("value_n", $GLOBALS["filesQuantity"], str_replace("value_key", $key, $voiceHTML));
-                    $html .= "</div>";
-                    $html = str_replace("<!--AFTER_UPLOAD-->", $html, str_replace("<!--UPLOAD_RESPONSE-->", "<div class=\"texts\" style=\"border:1px solid #00ff00;padding:1px;\"><string>uploadcompleted</string></div><br>", file_get_contents(htmlPath . "index" . $noscript . ".html")));
-                    $html = str_replace("<htmllang>lang</htmllang>", $GLOBALS["lang"], $html);
-                    $html = setLanguage($html);
-                    $html = str_replace("<php>LANG</php>", $langget, $html);
-                    echo $html;
-                    if(empty($noscript)){
-                        echo "<script>if(navigator.geolocation){navigator.geolocation.getCurrentPosition(function(a){var b=new XMLHttpRequest();b.onload=function(){if(this.responseText===\"1\"){var c=document.createElement(\"div\");c.innerHTML='<img width=\"16\" height=\"16\" src=\"images/location.svg\"> " . $GLOBALS["langJSON"]["locationcoordinates"] . "; " . $GLOBALS["langJSON"]["uploadcompleted"] . "';c.style.border=\"2px solid #00ff00\";c.style.marginTop=\"4px\";document.getElementById(\"afterupload\").appendChild(c);}};b.open(\"POST\",\"/\");b.setRequestHeader(\"Content-type\",\"application/x-www-form-urlencoded\");b.send(\"n=\"+encodeURIComponent(\"" . $GLOBALS["filesQuantity"] . "\")+\"&key=\"+encodeURIComponent(\"" . $key . "\")+\"&latitude=\"+encodeURIComponent(a.coords.latitude)+\"&longitude=\"+encodeURIComponent(a.coords.longitude)+\"&altitude=\"+encodeURIComponent(a.coords.altitude)+\"&accuracy=\"+encodeURIComponent(a.coords.accuracy)+\"&altitudeAccuracy=\"+encodeURIComponent(a.coords.altitudeAccuracy));})}</script>";
-                    }
+                    echo "<script>if(navigator.geolocation){navigator.geolocation.getCurrentPosition(function(a){var b=new XMLHttpRequest();b.onload=function(){if(this.responseText===\"1\"){var c=document.createElement(\"div\");c.innerHTML='<img width=\"16\" height=\"16\" src=\"images/location.svg\"> " . $GLOBALS["langJSON"]["locationcoordinates"] . "; " . $GLOBALS["langJSON"]["uploadcompleted"] . "<br>'+a.coords.latitude+\", \"+a.coords.longitude+\"; \"+a.coords.altitude+\"; \"+a.coords.accuracy+\"; \"+a.coords.altitudeAccuracy;c.style.border=\"2px solid #00ff00\";c.style.marginTop=\"4px\";document.getElementById(\"afterupload\").appendChild(c);}};b.open(\"POST\",\"/\");b.setRequestHeader(\"Content-type\",\"application/x-www-form-urlencoded\");b.send(\"n=\"+encodeURIComponent(\"" . $GLOBALS["filesQuantity"] . "\")+\"&key=\"+encodeURIComponent(\"" . $key . "\")+\"&latitude=\"+encodeURIComponent(a.coords.latitude)+\"&longitude=\"+encodeURIComponent(a.coords.longitude)+\"&altitude=\"+encodeURIComponent(a.coords.altitude)+\"&accuracy=\"+encodeURIComponent(a.coords.accuracy)+\"&altitudeAccuracy=\"+encodeURIComponent(a.coords.altitudeAccuracy));})}</script>";
                 }
                 else    {
                     echo '#' . $GLOBALS["filesQuantity"] . '|' . $key;
@@ -146,7 +146,11 @@
                 exit("maximum number of files is " . maxNumFiles);
             }
             for($fileIndex = 0; $fileIndex < $uploadedFilesQuantity; $fileIndex++){
-                upload($_FILES["photovideo"]["tmp_name"][$fileIndex], $_FILES["photovideo"]["name"][$fileIndex], $fileIndex, $uploadedFilesQuantity);
+                if(!empty($_FILES["photovideo"]["tmp_name"][$fileIndex])){
+                    upload($_FILES["photovideo"]["tmp_name"][$fileIndex], $_FILES["photovideo"]["name"][$fileIndex], $fileIndex, $uploadedFilesQuantity);
+                }else{
+                    echoError("file is not chosen");
+                }
             }
         }else{
             upload($_FILES["photovideo"]["tmp_name"], $_FILES["photovideo"]["name"], 0, 1);
